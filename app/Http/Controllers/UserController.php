c44ffef5bc5f8f4da97755f74683445ddf42b816
Victor <victorhenriquesan@outlook.com>
2024-09-14 14:13:05 -0300

    UserController Criado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create(){
+        return view('auth.register');
+    },
+}
+
+    public function store(Request $request)
+{
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8|confirmed',
+    ]);
+
+    User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+
+    return redirect()->route('login')->with('success', 'Usuário criado com sucesso!');
 }
