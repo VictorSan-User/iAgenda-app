3ebb05d788d65a0e5fee5accdfa2fcb91ba15a46
Victor <victorhenriquesan@outlook.com>
2024-09-14 20:10:46 -0300

    Incrementacao Calendario

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,72 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <head>
+                    <meta charset="UTF-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                 
+                    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+                    <title>Calendario</title>
+                    </head>
+
+                    <body>
+                    <div id="container">
+                        <div id="header">
+                            <div id="monthDisplay"></div>
+
+                            <div>
+                            <button id="backButton">Voltar</button>
+                            <button id="nextButton">Próximo</button>
+                            </div>
+                            
+                        </div>
+
+                        <div id="weekdays">
+                            <div>Domingo</div>
+                            <div>Segunda-feira</div>
+                            <div>Terça-feira</div>
+                            <div>Quarta-feira</div>
+                            <div>Quinta-feira</div>
+                            <div>Sexta-feira</div>
+                            <div>Sábado</div>
+                        </div>
+
+
+                        <!-- div dinamic -->
+                        <div id="calendar" ></div>
+
+                    
+                    </div>
+
+                    <div id="newEventModal">
+                        <h2>New Evente</h2>
+
+                        <input id="eventTitleInput" placeholder="Event Title"/>
+
+                        <button id="saveButton"> Salvar</button>
+                        <button id="cancelButton">Cancelar</button>
+                    </div>
+
+                    <div id="deleteEventModal">
+                        <h2>Evento</h2>
+
+                        <div id="eventText"></div><br>
+
+
+                        <button id="deleteButton">Deletar</button>
+                        <button id="closeButton">Fechar</button>
+                    </div>
+
+                    <div id="modalBackDrop"></div>
+
+
+                    <script src="{{ asset('js/mainCalendar.js') }}"></script>
+                    
+                    </body>
+                    </html>
                 </div>
             </div>
         </div>
